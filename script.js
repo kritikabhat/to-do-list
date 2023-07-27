c3a4edab52a2f9a235c9c77ad97ec5740a28b3c1
kritika <kbhat3@asu.edu>
2023-07-27 17:12:38 +0530

    feat: can change task progress

--- script.js
+++ script.js
@@ -4,28 +4,27 @@ const myModal = document.getElementById("myModal")
 const overlay = document.getElementById("overlay")
 const addBtn = document.getElementById("addBtn")
 const submitBtn = document.getElementById("submitBtn")
+const leftBarBtn = document.getElementById("leftBarBtn")
+const leftBarDiv = document.getElementById("leftBarDiv")
 const tasks = []
 
 // Force JS date format at the end so all dates are consistant.
 // 2002-12-22 is the format that modal puts in the div right now
 // change "date" to deadline everywhere to avoid confusion with keyword "date". Later.
 
-const deleteTask = () => {
+const completeTask = () => { // Before this, set up the Archive section
     
 }
-// Should ideally be able to change the Task, deadline since we already will be adding button for progress
+// Should ideally be able to change the Task, deadline (nothing for progress required)
 const updateTasks = () => {
     
-}
-const handleArchiveBtn = () => {
-    // This Archive button is in left nav bar
-
 }
 const handleDoneCheckBox = () => {
     // When Done is clicked, the row should move to Archive
 }
-const changeProgress = () => {
-    // when you click the Progress div on any row, change its color. Similar to the remove on library
+const handleArchiveBtn = () => {
+    // This Archive button is in left nav bar
+
 }
 
 const taskFactory = (task, date, progress) => { // working.
@@ -35,7 +34,6 @@ const closeAddTaskModal = () => { // working.
     myModal.classList.add("hidden")
     overlay.classList.add("hidden")
 }
-
 const resetTaskModal = (task, date, progress) => { // working.
     task.value = ""
     date.value = ""
@@ -62,6 +60,10 @@ const addTaskToDom = (taskRow) => { // working.
     date.classList.add("date")
     date.textContent = taskRow.date
     progress.classList.add("progress")
+    if (taskRow.progress === "Not Started")
+        progress.classList.add("notStarted")
+    else
+        progress.classList.add("inProgress")
     progress.textContent = taskRow.progress // progress.value won't work cause the thing is not a dropdown in the final row. Need to remove dropdown to avoid confusion or add it there as well
     done.classList.add("done")
     // done.checked = false // This will always be false when adding a new value. No need to default, already defaults to unchecked
@@ -72,7 +74,6 @@ const addTaskToDom = (taskRow) => { // working.
     newRow.appendChild(done)
     tasksDisplay.appendChild(newRow)
 }
-
 const handleSubmitBtn = () => { // working.
     const task = document.getElementById("task")
     const date = document.getElementById("date")
@@ -91,18 +92,44 @@ const handleAddBtn = () => { // working. // Does a method make sense?
     myModal.classList.remove("hidden")
     overlay.classList.remove("hidden")
 }
+const changeProgress = (e) => { // Working // But very ugly code?
+    if (e.target.classList[0] === "progress" && e.target.parentNode.classList[1] !== "rowHeader") {
+        if (e.target.textContent === "In Progress") {
+            e.target.textContent = "Not Started"
+            e.target.classList.remove("inProgress")
+            e.target.classList.add("notStarted")
+        } else {
+            e.target.textContent = "In Progress"
+            e.target.classList.remove("notStarted")
+            e.target.classList.add("inProgress")
+        } 
+    } else return
+}
 
+
+
+const handleLeftNavBtn = () => { // Working on this right now
+
+}
 const setUpPage  = () => {
-    addTask("Meet Suzy", "2023-08-22", "In Progress")
+    addTask("Meet Suzy", "2023-08-22", "Not Started")
 
     addBtn.addEventListener("click", handleAddBtn)
 
+    tasksDisplay.addEventListener("click", (e) => {
+        changeProgress(e) 
+    })
+
+    leftBarBtn.addEventListener("click", () => {
+        console.log("Update the leftNavBar")
+        leftBarDiv.setAttribute("id", "leftBarDiv")
+    })
 
 
 
     // It constantly checks for EVERY key even if I add it to a different method. Sad. lol.
     document.addEventListener("keydown", (e) => {
-        console.log(e.key)
+        console.log(e.key) // Delete this at the end
         if (e.key === "Escape") closeAddTaskModal()
     })
 
