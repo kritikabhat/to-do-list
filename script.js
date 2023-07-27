eab93450f3ec82e3e69473867a1ce477fd07429f
kritika <kbhat3@asu.edu>
2023-07-27 21:32:36 +0530

    feat: added archive page

--- script.js
+++ script.js
@@ -4,26 +4,13 @@ const myModal = document.getElementById("myModal")
 const overlay = document.getElementById("overlay")
 const addBtn = document.getElementById("addBtn")
 const submitBtn = document.getElementById("submitBtn")
-const leftBarBtn = document.getElementById("leftBarBtn")
-const leftBarDiv = document.getElementById("leftBarDiv")
+const projectBtn = document.getElementById("projectBtn")
+const archiveBtn = document.getElementById("archiveBtn")
+const homeBtn = document.getElementById("homeBtn")
+const archiveDiv = document.getElementById("archiveDiv")
 const tasks = []
 
-// Force JS date format at the end so all dates are consistant.
-// 2002-12-22 is the format that modal puts in the div right now
-// change "date" to deadline everywhere to avoid confusion with keyword "date". Later.
-
-const completeTask = () => { // Before this, set up the Archive section
-    
-}
-// Should ideally be able to change the Task, deadline (nothing for progress required)
-const updateTasks = () => {
-    
-}
-const handleDoneCheckBox = () => {
-    // When Done is clicked, the row should move to Archive
-}
-const handleArchiveBtn = () => {
-    // This Archive button is in left nav bar
+const handleProjectBtn = () => { // Open up new section again
 
 }
 
@@ -34,117 +21,131 @@ const closeAddTaskModal = () => { // working.
     myModal.classList.add("hidden")
     overlay.classList.add("hidden")
 }
-const resetTaskModal = (task, date, progress) => { // working.
+const resetTaskModal = (task, date, progressCheckBox) => { // working.
     task.value = ""
     date.value = ""
-    progress.value = ""
+    progressCheckBox.value = ""
     task.placeholder = "Task"
-    progress.value = "notStarted"
+    progressCheckBox.value = "notStarted"
 }
 const addTask = (task, date, progress) => { // working.
     const taskRow = taskFactory(task, date, progress)
     tasks.push(taskRow)
     addTaskToDom(taskRow)
 }
-const addTaskToDom = (taskRow) => { // working.
-    const newRow = document.createElement("div")
-    const task = document.createElement("div")
-    const date = document.createElement("div")
-    const progress = document.createElement("div")
-    const done = document.createElement("input")
-    done.setAttribute("type", "checkbox")
-
-    newRow.classList.add("row")
-    task.classList.add("task")
-    task.textContent = taskRow.task
-    date.classList.add("date")
-    date.textContent = taskRow.date
-    progress.classList.add("progress")
-    if (taskRow.progress === "Not Started")
-        progress.classList.add("notStarted")
-    else
-        progress.classList.add("inProgress")
-    progress.textContent = taskRow.progress // progress.value won't work cause the thing is not a dropdown in the final row. Need to remove dropdown to avoid confusion or add it there as well
-    done.classList.add("done")
-    // done.checked = false // This will always be false when adding a new value. No need to default, already defaults to unchecked
-
-    newRow.appendChild(task)
-    newRow.appendChild(date)
-    newRow.appendChild(progress)
-    newRow.appendChild(done)
-    tasksDisplay.appendChild(newRow)
-}
 const handleSubmitBtn = () => { // working.
     const task = document.getElementById("task")
     const date = document.getElementById("date")
-    const progress = document.getElementById("progress")
- 
+    const progressCheckBox = document.getElementById("progressCheckBox")
+    let progress
     if (task.value === "" || date.value === null || date.value === undefined) {
         alert("Both Task and Date are mandatory fields.")
         return
     }
-
-    addTask(task.value, date.value, progress.value)
+    progress = (progressCheckBox.value === "notStarted") ? "Not Started" : "In Progress"
+    addTask(task.value, date.value, progress)
     closeAddTaskModal()
-    resetTaskModal(task, date, progress)
+    resetTaskModal(task, date, progressCheckBox)
 }
 const handleAddBtn = () => { // working. // Does a method make sense?
     myModal.classList.remove("hidden")
     overlay.classList.remove("hidden")
 }
 const changeProgress = (e) => { // Working // But very ugly code?
-    if (e.target.classList[0] === "progress" && e.target.parentNode.classList[1] !== "rowHeader") {
-        if (e.target.textContent === "In Progress") {
-            e.target.textContent = "Not Started"
-            e.target.classList.remove("inProgress")
-            e.target.classList.add("notStarted")
-        } else {
-            e.target.textContent = "In Progress"
-            e.target.classList.remove("notStarted")
-            e.target.classList.add("inProgress")
-        } 
-    } else return
+    if (e.target.textContent === "In Progress") {
+        e.target.textContent = "Not Started"
+        e.target.classList.remove("inProgress")
+        e.target.classList.add("notStarted")
+    } else {
+        e.target.textContent = "In Progress"
+        e.target.classList.remove("notStarted")
+        e.target.classList.add("inProgress")
+    } 
 }
+const handleHomeBtn = () => { // working
+    tasksDisplay.classList.add("tasksDisplay")
+    tasksDisplay.classList.remove("hidden")
+    archiveDiv.classList.add("hidden")
+    archiveDiv.classList.remove("tasksDisplay")
+}
+const handleArchiveBtn = () => { // Working
+    tasksDisplay.classList.add("hidden")
+    tasksDisplay.classList.remove("tasksDisplay")
+    archiveDiv.classList.add("tasksDisplay")
+}
+const handleDoneCheckBox = (e) => { // Working. "Hidden" on the row, move to Archive, delete from "tasks" storage
+    if (e.target.checked === true) { 
+        e.target.parentNode.classList.add("hidden")
+        e.target.parentNode.classList.remove("row")  
+    }
+    const children = e.target.parentNode.children
+    let arr = []
 
+    for (const child of children) { // arr=["Clean", "2023-09-17", "In Progress", "some undefined crap i dont care about"]
+        arr.push(child.textContent)
+    }
+    const taskRow = taskFactory(arr[0], arr[1], "Complete")
 
+    // Delete from store
+    for (let i = 0; i < tasks.length; i++) {
+       if (tasks[i].task === taskRow.task) tasks.splice(i, 1)
+    }
+    moveToArchive(taskRow)
+}
+const moveToArchive = (taskRow) => { // Working. Moves rows to Archive section. 
+    const newRow = document.createElement("div")
+    let arr = ["task", "date", "progress"]
+    let taskDetail
+    arr.forEach(item => {
+        taskDetail = document.createElement('div')
+        taskDetail.classList.add(item)
+        if (item === "progress")
+            taskDetail.classList.add("completed")
+        taskDetail.textContent = taskRow[item]
+        newRow.appendChild(taskDetail)
+    });
+    newRow.classList.add("row")
+    archiveDiv.appendChild(newRow)
+}
+const addTaskToDom = (taskRow) => { // Working.
+    const newRow = document.createElement("div")
+    let arr = ["task", "date", "progress"]
+    let taskDetail
 
-const handleLeftNavBtn = () => { // Working on this right now
+    arr.forEach(item => {
+        taskDetail = document.createElement('div')
+        taskDetail.classList.add(item)
+        if (item === "progress") {
+            taskDetail.classList.add((taskRow[item] === "Not Started") ? "notStarted" : "inProgress")
+        }
+        taskDetail.textContent = taskRow[item]
+        newRow.appendChild(taskDetail)
+    });
 
+    taskDetail = document.createElement("input") // For done
+    taskDetail.setAttribute("type", "checkbox")
+    taskDetail.classList.add("done")
+    newRow.appendChild(taskDetail)
+
+    newRow.classList.add("row")
+    tasksDisplay.appendChild(newRow)
 }
 const setUpPage  = () => {
-    addTask("Meet Suzy", "2023-08-22", "Not Started")
-
+    addTask("Meet Suzy", "2023-08-22", "Not Started") // Setting up with one task
     addBtn.addEventListener("click", handleAddBtn)
-
-    tasksDisplay.addEventListener("click", (e) => {
-        changeProgress(e) 
-    })
-
-    leftBarBtn.addEventListener("click", () => {
-        const logoText = document.getElementById("logoText")
-        const archive = document.getElementById("archive")
-        const project = document.getElementById("project")
-        if (leftBarDiv.id === "leftBarDiv") {
-            leftBarDiv.setAttribute("id", "expandedLeftBar")
-            logoText.classList.remove("hidden")
-            archive.classList.remove("hidden")
-            project.classList.remove("hidden")
-        } else {
-            leftBarDiv.setAttribute("id", "leftBarDiv")
-            logoText.classList.add("hidden")
-            archive.classList.add("hidden")
-            project.classList.add("hidden")
-        }  
-    })
-
-
-
-    // It constantly checks for EVERY key even if I add it to a different method. Sad. lol.
-    document.addEventListener("keydown", (e) => {
-        console.log(e.key) // Delete this at the end
-        if (e.key === "Escape") closeAddTaskModal()
-    })
-
+    homeBtn.addEventListener("click", handleHomeBtn)
+    archiveBtn.addEventListener("click", handleArchiveBtn)
     submitBtn.addEventListener("click", handleSubmitBtn)
+    tasksDisplay.addEventListener("click", (e) => {
+        if (e.target.classList[0] === "progress" && e.target.parentNode.classList[0] !== "rowHeader")
+            changeProgress(e)
+        else if (e.target.classList[0] === "done" && e.target.parentNode.classList[0] !== "rowHeader")
+            handleDoneCheckBox(e)
+        else
+            return
+    })
+    document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") closeAddTaskModal()
+    })   
 }
 setUpPage()
